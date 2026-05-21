db56289be9b89ad7720b9961615dfc41d3c95b97
Robb-Designs <rtlee@aggies.ncat.edu>
2026-05-21 14:31:29 -0400

    add user registration endpoint with password hashing and duplicate username check

--- server.js
+++ server.js
@@ -5,6 +5,9 @@ const mongoose = require('mongoose');
 const bcrypt = require('bcrypt');
 const jwt = require('jsonwebtoken');
 
+//User Model
+const User = require('./models/User.js');
+
 require('dotenv').config();
 const PORT = process.env.PORT;
 const URI = process.env.MONGO_URI;
@@ -43,6 +46,26 @@ mongoConnection();
 //ROUTES----------------------------------------------------------------------------------------------------------------------
 app.get('/', (req, res) => {
     res.send('Testing testing...')
+})
+
+app.post('/register', async (req, res) => {
+    try {
+        const { username, password } = req.body;
+        const savedUsername = await User.findOne({ username });
+
+        if (!savedUsername) {
+            // hashing and salting
+            const salt = await bcrypt.genSalt(10);
+            const hashedPassword = await bcrypt.hash(password, salt);
+            const newUser = new User({ username, password: hashedPassword });
+            await newUser.save();
+            return res.status(201).json({ message: 'User registered successfully' });
+        } else {
+            return res.status(400).json({ message: 'Username already exists' });
+        }
+    } catch (error) {
+        return res.status(500).json({ message: 'Server error', error: error.message });
+    }
 })
 
 
