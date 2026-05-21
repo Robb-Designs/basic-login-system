4f136fa910a22f97fbe186bfdf372fc69073af61
Robb-Designs <rtlee@aggies.ncat.edu>
2026-05-21 10:35:44 -0400

    shorted variable name

--- server.js
+++ server.js
@@ -1,5 +1,12 @@
 //DEPENDENCIES----------------------------------------------------------------------------------------------------------------------
+const express = require('express');
+const app = express();
+const mongoose = require('mongoose');
+const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
 
+require('dotenv').config();
+const PORT = process.env.PORT;
 
 
 
